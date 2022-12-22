327ddb7b0f5ff5825b303a84096479db59d50531
TwistyTea <Adinathuparkar23@gmail.com>
2022-12-22 19:58:40 +0530

    fix transition duration

--- components/Layout.js
+++ components/Layout.js
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
                         initial={{ opacity: 0, x: '-100%' }}
                         animate={{ opacity: 1, x: 0 }}
                         exit={{ opacity: 0, x: '100%' }}
-                        transition={{ duration: 2 }}
+                        transition={{ duration: 0.25 }}
                     >
                         {children}
                     </motion.main>
